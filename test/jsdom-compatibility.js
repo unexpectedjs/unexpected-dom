83f719a80c65a7c3ccd0b272a01850d83122d0e9
Peter Müller <munter@fumle.dk>
2015-04-27 22:12:27 +0200

    Added workaround for unfixed jsdom bug tmpvar/jsdom#1107

---
+++ test/jsdom-compatibility.js
@@ -0,0 +1,31 @@
+/*global describe, it*/
+var unexpected = require('unexpected'),
+    unexpectedDom = require('../lib/index'),
+    jsdom = require('jsdom');
+
+var expect = unexpected.clone().installPlugin(unexpectedDom);
+expect.output.installPlugin(require('magicpen-prism'));
+
+expect.addAssertion('to inspect as [itself]', function (expect, subject, value) {
+  var originalSubject = subject;
+  if (typeof subject === 'string') {
+    subject = jsdom.jsdom('<!DOCTYPE html><html><head></head><body>' + subject + '</body></html>').body.firstChild;
+  }
+  if (this.flags.itself) {
+    if (typeof originalSubject === 'string') {
+      expect(expect.inspect(subject).toString(), 'to equal', originalSubject);
+    } else {
+      throw new Error('subject must be given as a string when expected to inspect as itself');
+    }
+  } else {
+    expect(expect.inspect(subject).toString(), 'to equal', value);
+  }
+});
+
+describe('jsdom bug compatibility', function () {
+  it('should work without issue #1107 fixed', function() {
+    // https://github.com/tmpvar/jsdom/issues/1107
+    expect('<select><option value="foo">bar</option></select>', 'to inspect as itself');
+    expect('<form><p>foo</p></form>', 'to inspect as itself');
+  });
+});
