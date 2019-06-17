90fd112427bac85072960634b051d717e877d19e
Sune Simonsen <sune@we-knowhow.dk>
2019-06-17 12:45:45 +0200

    Make sure we nest the snapshot error message.

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,21 +11,23 @@ it.skipIf = function(bool, descr, block) {
 expect.addAssertion(
   '<function> to error satisfying <assertion>',
   (expect, cb) =>
-    expect(cb, 'to error').then(err =>
-      expect.shift(
+    expect(cb, 'to error').then(err => {
+      expect.errorMode = 'nested';
+      return expect.shift(
         err.isUnexpected ? err.getErrorMessage('text').toString() : err.message
-      )
-    )
+      );
+    })
 );
 
 expect.addAssertion(
   '<function> to throw an error satisfying <assertion>',
   (expect, cb) =>
-    expect(cb, 'to throw').then(err =>
-      expect.shift(
+    expect(cb, 'to throw').then(err => {
+      expect.errorMode = 'nested';
+      return expect.shift(
         err.isUnexpected ? err.getErrorMessage('text').toString() : err.message
-      )
-    )
+      );
+    })
 );
 
 expect.addAssertion(
