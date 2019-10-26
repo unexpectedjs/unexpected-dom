1869b96e371d72cf4e0bd5614d69c50f14e2f6ed
Andreas Lind <andreaslindpetersen@gmail.com>
2019-10-26 15:07:31 +0200

    Don't use the typeless addAssertion syntax (silences deprecation warning)

--- test/jsdom-compatibility.js
+++ test/jsdom-compatibility.js
@@ -5,25 +5,28 @@ const jsdom = require('jsdom');
 const expect = unexpected.clone().installPlugin(unexpectedDom);
 expect.output.installPlugin(require('magicpen-prism'));
 
-expect.addAssertion('to inspect as [itself]', function(expect, subject, value) {
-  const originalSubject = subject;
-  if (typeof subject === 'string') {
-    subject = new jsdom.JSDOM(
-      `<!DOCTYPE html><html><head></head><body>${subject}</body></html>`
-    ).window.document.body.firstChild;
-  }
-  if (this.flags.itself) {
-    if (typeof originalSubject === 'string') {
-      expect(expect.inspect(subject).toString(), 'to equal', originalSubject);
-    } else {
-      throw new Error(
-        'subject must be given as a string when expected to inspect as itself'
-      );
+expect.addAssertion(
+  '<string|DOMNode> to inspect as [itself] <string?>',
+  function(expect, subject, value) {
+    const originalSubject = subject;
+    if (typeof subject === 'string') {
+      subject = new jsdom.JSDOM(
+        `<!DOCTYPE html><html><head></head><body>${subject}</body></html>`
+      ).window.document.body.firstChild;
     }
-  } else {
-    expect(expect.inspect(subject).toString(), 'to equal', value);
+    if (this.flags.itself) {
+      if (typeof originalSubject === 'string') {
+        expect(expect.inspect(subject).toString(), 'to equal', originalSubject);
+      } else {
+        throw new Error(
+          'subject must be given as a string when expected to inspect as itself'
+        );
+      }
+    } else {
+      expect(expect.inspect(subject).toString(), 'to equal', value);
+    }
   }
-});
+);
 
 describe('jsdom bug compatibility', () => {
   it('should work without issue #1107 fixed', () => {
