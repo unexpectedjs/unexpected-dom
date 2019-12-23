42636fc8b910d3fb3519cb5e7e9dd85b98e42f94
Alex J Burke <alex@alexjeffburke.com>
2019-12-23 17:17:34 +0100

    Move "to have test id" to a separate file.

--- test/index.spec.js
+++ test/index.spec.js
@@ -622,60 +622,6 @@ describe('unexpected-dom', () => {
     });
   });
 
-  describe('to have test id', () => {
-    it('should pass if the element have the given test id', () => {
-      const document = parseHtmlDocument(
-        '<!DOCTYPE html><html><body><div data-test-id="foo"></div></body></html>'
-      );
-
-      expect(document.body.firstChild, 'to have test id', 'foo');
-    });
-
-    it('should fail if the element does not have the given test id', () => {
-      const document = parseHtmlDocument(
-        '<!DOCTYPE html><html><body><div data-test-id="foo"></div></body></html>'
-      );
-
-      expect(
-        () => {
-          expect(document.body.firstChild, 'to have test id', 'bar');
-        },
-        'to throw an error satisfying to equal snapshot',
-        expect.unindent`
-        expected <div data-test-id="foo"></div> to have test id 'bar'
-          expected <div data-test-id="foo"></div> to match '[data-test-id="bar"]'
-      `
-      );
-    });
-  });
-
-  describe('not to have test id', () => {
-    it('should pass if the element does not have the given test id', () => {
-      const document = parseHtmlDocument(
-        '<!DOCTYPE html><html><body><div data-test-id="foo"></div></body></html>'
-      );
-
-      expect(document.body.firstChild, 'not to have test id', 'bar');
-    });
-
-    it('should fail if the element have the given test id', () => {
-      const document = parseHtmlDocument(
-        '<!DOCTYPE html><html><body><div data-test-id="foo"></div></body></html>'
-      );
-
-      expect(
-        () => {
-          expect(document.body.firstChild, 'not to have test id', 'foo');
-        },
-        'to throw an error satisfying to equal snapshot',
-        expect.unindent`
-        expected <div data-test-id="foo"></div> not to have test id 'foo'
-          expected <div data-test-id="foo"></div> not to match '[data-test-id="foo"]'
-      `
-      );
-    });
-  });
-
   describe('when parsed as HTML', () => {
     const htmlSrc = '<!DOCTYPE html><html><body class="bar">foo</body></html>';
     it('should parse a string as a complete HTML document', () => {
