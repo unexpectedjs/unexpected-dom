466d1c50468a871f2babc66d272dc9d05135ae86
Alex J Burke <alex@alexjeffburke.com>
2019-12-23 20:16:24 +0100

    Use IE11 compatible syntax for the common/browser custom assertion.

--- test/common/browser.js
+++ test/common/browser.js
@@ -6,11 +6,12 @@ expect.use(window.unexpectedSnapshot);
 
 expect.addAssertion(
   '<function> to throw an error satisfying <assertion>',
-  (expect, cb) =>
-    expect(cb, 'to throw').then(err => {
+  function(expect, cb) {
+    return expect(cb, 'to throw').then(function(err) {
       expect.errorMode = 'nested';
       return expect.shift(
         err.isUnexpected ? err.getErrorMessage('text').toString() : err.message
       );
-    })
+    });
+  }
 );
