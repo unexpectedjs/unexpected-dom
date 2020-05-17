3a5b2621179d709db70577a40ab0676efb215426
Andreas Lind <andreaslindpetersen@gmail.com>
2020-05-17 09:33:15 +0200

    Add another helper for creating an actual document

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,4 +1,4 @@
-/* global unexpected:true, expect:true, jsdom:true, createElement:true */
+/* global unexpected:true, expect:true, jsdom:true, createElement:true, createDocument:true */
 /* eslint no-unused-vars: "off" */
 
 if (typeof expect === 'undefined') {
@@ -8,18 +8,29 @@ if (typeof expect === 'undefined') {
 unexpected = expect;
 unexpected.output.preferredWidth = 80;
 
-createElement = function createElement(html) {
-  var root = document.createElement('div');
-
-  root.innerHTML = html
+function sanitizeHtml(html) {
+  return html
     .replace(/^\s+|\s+$/gm, '')
     .replace(/\s*\n\s*</gm, '<')
     .replace(/>\s*\n\s*/gm, '>')
     .trim();
+}
+
+createElement = function createElement(html) {
+  var root = document.createElement('div');
+
+  root.innerHTML = sanitizeHtml(html);
 
   return root.firstChild;
 };
 
 /* eslint-disable no-global-assign */
 window = new jsdom.JSDOM().window;
+document = window.document;
+
+createDocument = function createDocument(html) {
+  return new jsdom.JSDOM(`<body>${sanitizeHtml(html)}</body>`).window.document;
+};
+
+/* eslint-disable no-global-assign */
 document = window.document;
